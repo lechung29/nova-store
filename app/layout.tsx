25914e50c4aff4ca06646b4cdb60d8d68a392382
Killian Le <86923962+lechung29@users.noreply.github.com>
2026-06-10 14:13:53 +0700

    update icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,9 +52,12 @@ export const metadata: Metadata = {
     },
 
     icons: {
-        icon: "/icon.png",
-        shortcut: "/favicon-16x16.png",
-        apple: "/apple-touch-icon.png",
+        icon: [
+            { url: "/favicon.ico", sizes: "32x32" },
+            { url: "/favicon-192x192.png", sizes: "192x192", type: "image/png" },
+        ],
+        apple: [{ url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }],
+        shortcut: "/favicon.ico",
     },
 
     robots: {
